4c04ec8b31120af83f5a32bd2a2f825bbe1867fa
Nikushatavartkiladze13 <nikatavartkiladze9@icloud.com>
2025-02-27 16:09:25 +0400

    second commit

--- day_01/homework/hw.py
+++ day_01/homework/hw.py
@@ -11,4 +11,4 @@ height = 175,4            #float ტიპის (ათწილადი)
 life = "khuloshi"
 knows_programming = True   #true or folse
 
-print("me var nika tavartkiladze" + " " + age + " "  +  "wlis" + " " + "me davibade" + " " + birth +" " + "wles"+ " " + "me simagleshi var" + " " + height + " " + "vcxovrob" + " " + life)
+print("me var nika tavartkiladze" + " " + str(age) + " "  +  "wlis" + " " + "me davibade" + " " + birth +" " + "wles"+ " " + "me simagleshi var" + " " + str(height) + " " + "vcxovrob" + " " + life)
